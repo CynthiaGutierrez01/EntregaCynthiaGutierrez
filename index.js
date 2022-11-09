d9335613a2d587a75befb8217b6790012da2e9ed
Cynthia Gutierrez <cyn@goicoa.com.ar>
2022-11-09 13:50:37 -0300

    9-11

--- index.js
+++ index.js
@@ -13,27 +13,27 @@ alert("Bienvenido/a "+ nombreCliente + " a La Alfajorera");
 
 // Do/While - If/else
 
-
+function inicio(){
     do {
-        cobertura =parseInt(prompt (nombreCliente + " ingrese qué cobertura le gustaría. Para Chocolate Negro, escriba negro; para Chocolate Blanco, escriba blanco"));
-        if (cobertura = "negro") {
+        cobertura =prompt (nombreCliente + " ingrese qué cobertura le gustaría. Para Chocolate Negro, escriba negro; para Chocolate Blanco, escriba blanco");
+        if (cobertura == "negro") {
             coberturaChocolateNegro();
-        } else if (cobertura = "blanco") {
+        } else if (cobertura == "blanco") {
             coberturaChocolateBlanco();
         }else {
         alert("Ingrese una opcion valida: " + cobertura + " No es una opcion valida");
         }
-    }while (cobertura != "negro" && cobertura != "blanco");
-    
-
+    }while (cobertura !== "negro" && cobertura !== "blanco");
+}
+inicio ();
 
 // Funciones
 function coberturaChocolateNegro(){
     alert ( nombreCliente + ' elegiste Chocolate Negro para la cobertura');
-        relleno =parseInt (prompt ("Ahora toca elegir el relleno! Escriba dulce de leche o nutella"));
-        if(relleno = "dulce de leche"){
+        relleno =prompt ("Ahora toca elegir el relleno! Escriba dulce de leche o nutella");
+        if(relleno == "dulce de leche"){
             rellenoDdL();
-        }else if(relleno = "nutella"){
+        }else if(relleno == "nutella"){
             rellenoNutella();
         } else {
             alert("Ingrese una opcion valida: " + relleno + " No es una opcion valida");
@@ -42,15 +42,15 @@ function coberturaChocolateNegro(){
 
 function coberturaChocolateBlanco(){
     alert ( nombreCliente + ' elegiste Chocolate Blanco para la cobertura');
-    relleno = parseInt(prompt ("Ahora toca elegir el relleno! Escriba dulce de leche o nutella"));
-    if(relleno  = "dulce de leche"){
+    relleno = prompt ("Ahora toca elegir el relleno! Escriba dulce de leche o nutella");
+    if(relleno  == "dulce de leche"){
         rellenoDdL();
-    }else if(relleno = "nutella"){
+    }else if(relleno == "nutella"){
         rellenoNutella();
     }else {
         alert("Ingrese una opcion valida: " + relleno + " \nNo es una opcion valida");
         coberturaChocolateBlanco();
-    }}
+        }}
 
 function rellenoDdL(){
     alert("Usted eligió el relleno con dulce de leche");
@@ -59,6 +59,8 @@ function rellenoDdL(){
 function rellenoNutella(){
     alert("Usted eligió el relleno con nutella")
 }
+
+
 
 // Arrays
 
@@ -78,28 +80,40 @@ console.log(Alfajor1);
 carrito.push(Alfajor1);
 console.log(carrito);
 
-let cantidad = prompt("Por favor ingese la cantidad de Alfajores que desea comprar")
 
 let precio = 200
+let cantidad = prompt("Por favor ingrese la cantidad de Alfajores que desea comprar")
 
 carrito.push({Alfajor1, cantidad, precio});
 console.log(carrito);
 
-final = prompt("desea seguir comprando?")
-
-while (final != "no"){
-    coberturaChocolateNegro
-    break;
-}
-
-while(final === "no"){
-    alert("Muchisimas gracias por habernos elegido!")
-    carrito.forEach((carritoFinal) => {
-        console.log(`producto: ${carritoFinal.Alfajor}, cantidad: ${carritoFinal.cantidad}`)
+function final(){
+    preguntaFinal = prompt( nombreCliente + " desea comprar más alfajores?");
+    if(preguntaFinal !=="no"){
+        inicio();
+    }else if(preguntaFinal ==="no")
+        alert("Muchisimas gracias por habernos elegido!")
+        carrito.forEach((carritoFinal) => {
+            console.log(`producto: ${carritoFinal.Alfajor}, cantidad: ${carritoFinal.cantidad}`)
     })
-break;
-
 }
+final();
+
+// inal = prompt("desea seguir comprando?")
+
+// while (final !== "no"){
+//     inicio();
+//     break;
+// }
+
+// while(final === "no"){
+//     alert("Muchisimas gracias por habernos elegido!")
+//     carrito.forEach((carritoFinal) => {
+//         console.log(`producto: ${carritoFinal.Alfajor}, cantidad: ${carritoFinal.cantidad}`)
+//     })
+// break;
+
+// }
 
 
 const total = carrito.reduce ((acc, el) => acc + el.precio * el.cantidad, 0)
